41f8fab49a8d38e7a3a8813cfcda1dd34e3a3a58
tbonelee <chanho0325@yonsei.ac.kr>
2021-04-12 04:00:53 +0900

    stack/queue

--- test4.c
+++ test4.c
@@ -1,21 +1,72 @@
-
 #include <stdio.h>
-#include <stdlib.h>
+
+void	push(int n, int stack[], int size);
+void	size(int *size);
+void	empty(int *size);
+void	top(int stack[], int *size);
 
 int main()
 {
-	long long rec = 0;
+	int N;
+	scanf("%d", &N);
+	char	str[6];
+	int temp;
+    int stack[10000];
+    int stacksize = -1;
 
-    for (unsigned long long i = 0; i <18446744073709551610; i++ )
-    {
-        int i = 0;
-        int j = 0;
-        i++;
-        j++;
-            for (unsigned long long i = 0; i <18446744073709551610; i++ )
-            {
-				rec++;
-            }
-    }
-    printf("Hello World!");
+	for (int i = 1; i <= N; i++)
+	{
+
+		scanf("%s", str);
+		if (str[0] == 'p' && str[1] == 'u')
+		{
+			scanf("%d", &temp);
+			stacksize++;
+			push(temp, stack, stacksize);
+		}
+		else if (str[0] == 'p' && str[2] == 'p')
+		{
+			if (stacksize == -1)
+				printf("-1\n");
+			else
+			{
+				printf("%d\n", stack[stacksize]);
+				stacksize--;
+			}
+		}
+		else if (str[0] == 's')
+			size(&stacksize);
+		else if (str[0] == 'e')
+			empty(&stacksize);
+		else if (str[0] == 't')
+			top(stack, &stacksize);
+	}
+
+	return (0);
+}
+
+void	push(int n, int stack[], int size)
+{
+	stack[size] = n;
+}
+
+void	size(int *size)
+{
+	printf("%d\n", *size + 1);
+}
+
+void	empty(int *size)
+{
+	if (*size == -1)
+		printf("1\n");
+	else
+		printf("0\n");
+}
+
+void	top(int stack[], int *size)
+{
+	if (*size == -1)
+		printf("-1\n");
+	else
+		printf("%d\n", stack[*size]);
 }
